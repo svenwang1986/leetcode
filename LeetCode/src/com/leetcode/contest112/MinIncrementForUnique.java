0e82bdbbaa8e4a9969c691684a18739f1d4474bb
sven <sven@DESKTOP-KCC0T27>
2018-11-25 21:02:38 +0800

    1

---
+++ LeetCode/src/com/leetcode/contest112/MinIncrementForUnique.java
@@ -0,0 +1,24 @@
+package com.leetcode.contest112;
+
+public class MinIncrementForUnique {
+
+	
+	public int minIncrementForUnique(int[] A) {
+        int [] arr = new int [40001];
+        int moveTimes = 0;
+        for(int i = 0; i< A.length;i++){
+            
+            if(arr[A[i]] == 0){
+                arr[A[i]] =1;
+            }else{
+                moveTimes ++ ;
+                A[i] ++;
+                i--;
+            }
+            
+        }
+        
+        return moveTimes;
+    }
+	
+}

6e820871cbe75cc3b54bceaffaba41a44a6fa245
sven <sd_wangshuai@163.com>
2018-11-26 14:46:57 +0800

    leetcode

--- LeetCode/src/com/leetcode/contest112/MinIncrementForUnique.java
+++ LeetCode/src/com/leetcode/contest112/MinIncrementForUnique.java
@@ -2,18 +2,31 @@ package com.leetcode.contest112;
 
 public class MinIncrementForUnique {
 
-	
-	public int minIncrementForUnique(int[] A) {
-        int [] arr = new int [40001];
+    public static void main(String[] args) {
+        int [] arr = {3,2,1,2,1,7};
+
+        int res = new MinIncrementForUnique().minIncrementForUnique(arr);
+
+        System.out.println("res = "+res);
+    }
+
+    public int minIncrementForUnique(int[] A) {
+        int [] arr = new int [50000];
         int moveTimes = 0;
         for(int i = 0; i< A.length;i++){
             
             if(arr[A[i]] == 0){
                 arr[A[i]] =1;
             }else{
-                moveTimes ++ ;
-                A[i] ++;
-                i--;
+
+                for (int j = A[i]+1; ; j++) {
+                    if (arr[j] ==0){
+                        arr[j] =1;
+                        moveTimes += j-A[i];
+                        break;
+                    }
+                }
+
             }
             
             
---
+++ LeetCode/src/com/leetcode/contest112/ValidateStackSequences.java
@@ -0,0 +1,48 @@
+package com.leetcode.contest112;
+
+import java.util.Stack;
+
+public class ValidateStackSequences {
+
+    public static void main(String[] args) {
+
+
+        int [] pushed = {1,0,3,2};
+
+        int [] popped = {0,1,2,3};
+
+        boolean res = validateStackSequences(pushed,popped);
+
+
+        System.out.println(res);
+
+
+    }
+    public static boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        //用一个栈模拟操作，
+        //判断栈相等就出栈，否则入栈
+
+        Stack<Integer> st = new Stack<Integer>();
+
+        for (int i = 0,j=0; i < pushed.length && j < popped.length; i++) {
+
+            st.push(pushed[i]);
+
+            while (!st.isEmpty() && st.peek() == popped[j]){
+
+                st.pop();
+
+                j++;
+
+            }
+
+        }
+
+        if (st.size() == 0) return true;
+
+
+        return false;
+    }
+
+}
